0dcf19a3803053b2e825e8b7dd7da31213fe678d
Brandon Xue <brandon_xue@hotmail.com>
2021-05-03 17:55:30 -0700

    Add loader for the csv

--- src/main.cu
+++ src/main.cu
@@ -23,64 +23,9 @@ int main(int argc, char *argv[]) {
     conv_2d_input(&kern1, 1, 1, PAD_VALID, &img, &out);
     printf("Output dimensions: %d, %d.\n", out.width, out.height);
     */
-    unsigned char img_in1[36] = {
-        1, 3, 3, 2, 1, 1,
-        1, 2, 2, 1, 1, 1,
-        0, 1, 2, 1, 1, 0,
-        0, 1, 1, 1, 0, 1,
-        0, 0, 0, 1, 0, 0,
-        1, 0, 1, 1, 1, 0
-    };
-    UChar2D conv_in1 = {.width = 6, .height = 6, .data = img_in1};
-
-    double kern1[9] = {
-         2,  2,  2,
-         0,  0,  0,
-        -2, -2, -2
-    };
-    Double2D kernel1 = {.width = 3, .height = 3, .data = kern1};
-    
-    // Valid padding with stride of 2 in both axes
-    // expect last row and col to be ignored, so output 2x2
-    double expected1[4] = {
-        8, 4,
-        6, 6
-    };
-
-    Double2D output1 = {.width = 0, .height = 0, .data = NULL};
-    conv_2d_input(&kernel1, 2, 2, PAD_VALID, &conv_in1, &output1);
-    printf("%lf %lf\n%lf %lf\n",
-           output1.data[0], output1.data[1],
-           output1.data[2], output1.data[3]
-    );
-    
-    unsigned char img2[16] = {
-        1, 3, 3, 2,
-        1, 2, 2, 1,
-        0, 1, 2, 1,
-        0, 1, 1, 1
-    };
-    UChar2D in2 = {.width = 4, .height = 4, .data = img2};
-
-    double ker2[16] = {
-         2,  2,  2,
-         0,  0,  0,
-        -2, -2, -2
-    };
-    Double2D kernel2 = {.width = 3, .height = 3, .data = ker2};
-
-    // Valid padding with stride of 1 in both axes
-    // output 2x2
-    double expected2[4] = {
-        8, 8,
-        6, 4
-    };
-
-    Double2D out2 = {.width = 0, .height = 0, .data = NULL};
-    conv_2d_input(&kernel2, 1, 1, PAD_VALID, &in2, &out2);
-    printf("%lf %lf\n%lf %lf\n",
-           out2.data[0], out2.data[1],
-           out2.data[2], out2.data[3]
-    );
+    InputLabel *inputs = load_chinese_mnist_info();
+    /*for (int i = 0; i < 14999; ++i) {
+        printf("Input file %s has label %d\n", inputs[i].input, inputs[i].label);
+    }*/
     return 0;
 }
